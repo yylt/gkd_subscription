22358a8dc5c48717daaac656778202ef87b4efba
yylt <yang8518296@163.com>
2026-06-17 14:59:41 +0800

    feat: 添加应用规则、全局配置和文档（压缩提交）

--- src/subscription.ts
+++ src/subscription.ts
@@ -5,11 +5,11 @@ import globalGroups from './globalGroups';
 
 export default defineGkdSubscription({
   id: 233,
-  name: 'Subscription',
+  name: '小哇的GKD订阅',
   version: 0,
-  author: 'author',
+  author: '小哇',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/yylt/gkd_subscription',
   categories,
   globalGroups,
   apps: await batchImportApps(`${import.meta.dirname}/apps`),
